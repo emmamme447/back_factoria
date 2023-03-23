bc5d61c68b5a2ed6b05382865437b89cdb62c4d8
CodeMincer <codemincer@gmail.com>
2023-03-23 13:45:26 +0100

    Entity Creation

---
+++ src/Entity/Comentarios.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ComentariosRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ComentariosRepository::class)]
+class Comentarios
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fecha = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $comentario = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): self
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    public function getComentario(): ?string
+    {
+        return $this->comentario;
+    }
+
+    public function setComentario(string $comentario): self
+    {
+        $this->comentario = $comentario;
+
+        return $this;
+    }
+}
